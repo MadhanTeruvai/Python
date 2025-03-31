1ab80a2f3d627defd9cfdcca087e357f1bc77cc8
MadhanTeruvai <Madhanteruvai@gmail.com>
2025-03-31 18:53:47 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,38 @@
+# To type multiple alt+shit+click on touchpad 
+# click end for last sentence
+
+            # national anthem
+print("Jana-gana-mana-adhinayaka, jaya he")
+print("Bharata-bhagya-vidhata")
+print("Panjaba-Sindha-Gujrata-Maharata-")
+print("Dravida-Utkala-Vanga")
+print("Vindhya-Himachala-Yamuna-Ganga")
+print("Uchhala-Jaladhi-taranga")
+print("Tava shubha name jage")
+
+print("Tava shubha ashisha mage")
+print("Gave tava jaya-gatha")
+print("Jana-gana-mangala-dayaka jaya he")
+print("Bharata-bhagya-vidhata.")
+print("Jaya he! Jaya he! Jaya he!")
+print("Jaya jaya jaya, jaya he!")
+
+# by using triple quotes
+print('''Jana-gana-mana-adhinayaka, jaya he
+Bharata-bhagya-vidhata
+Panjaba-Sindha-Gujrata-Maharata-
+Dravida-Utkala-Vanga
+Vindhya-Himachala-Yamuna-Ganga
+Uchhala-Jaladhi-taranga
+Tava shubha name jage
+
+Tava shubha ashisha mage
+Gave tava jaya-gatha
+Jana-gana-mangala-dayaka jaya he
+Bharata-bhagya-vidhata.
+Jaya he! Jaya he! Jaya he!
+Jaya jaya jaya, jaya he!''')
+
+# to view directory by using python
+import os
+print(os.listdir())
