9050bbcfe5908b95f54c7128fdca41f1a5c3a651
MadhanTeruvai <Madhanteruvai@gmail.com>
2025-03-31 18:55:14 +0530

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,39 @@
+# # 1. program to add 2 numbers
+# a=int(input("enter the first number"))
+# b=int(input("enter the second number"))
+# print('the total sum of mentioned inputs are',a+b)
+
+# # 2. program to make division
+# a=int(input("enter the first number"))
+# b=int(input("enter the second number"))
+# print('the reminder of mentioned inputs are',a/b)
+
+# # 3. find the datatype
+# c=67
+# d=6.87
+# e="good"
+# f=None
+# g=True
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+
+# # 4. use comparison operators to find out variable a is greater than b or not take a=56 and b=70 
+# a=56
+# b=70
+# print(a>=b)
+# print(a<=b)
+# print(a==b)
+# print(a!=b)
+
+# # 5. to find the average of two numbers
+# a=56
+# b=98
+# c=a+b
+# print("the average of two numbers that are considered is:",c/2)
+
+# 6. To find a square of a number
+# j=int(input("enter the value to find out the square of a number"))
+# print(j**2)
